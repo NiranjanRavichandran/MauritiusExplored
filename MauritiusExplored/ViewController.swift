e667d6e7ce87d5292525dc89565ff03a30f0abe5
Niranjan Ravichandran <niranjanravichandran@live.com>
2015-04-25 11:19:01 +0530

    Initial Commit

---
+++ MauritiusExplored/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MauritiusExplored
+//
+//  Created by Niranjan Ravichandran on 25/04/15.
+//  Copyright (c) 2015 Adavers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
